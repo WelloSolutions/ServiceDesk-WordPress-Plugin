fbd493e04ddabf8563e93806fcbcf5d5deb2c75d
Hemant <38558894+hemant9701@users.noreply.github.com>
2026-02-09 18:10:45 +0530

    Modify plugin header information
    Updated plugin metadata to reflect new author and description.

--- wello-servicedesk-api.php
+++ wello-servicedesk-api.php
@@ -1,11 +1,29 @@
 <?php
 /**
- * Plugin Name: Wello Service Desk API
- * Description: A plugin that integrates a React app.
- * Version: 1.0
- * Author: Hemant
- */
-
+ * Plugin Name: Wello Service Desk API
+ * Description: A Service Desk for field service companies.
+ * Version: 1.0
+ * Author: Wello
+ * Author URI: https://wello.solutions/
+ * Domain Path: /languages
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wello-servicedesk
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 function wello_servicedesk_enqueue_scripts() {
     $plugin_dir_url = plugin_dir_url(__FILE__);
     $plugin_dir_path = plugin_dir_path(__FILE__);
